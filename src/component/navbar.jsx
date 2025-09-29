c5d426860e07705b5450bc1c13c42d912350cba7
Kartik Sharma <poojan4604@gmail.com>
2025-09-29 23:14:42 +0530

    nav bar setup
    still incomplete

---
+++ src/component/navbar.jsx
@@ -0,0 +1,11 @@
+export default function Navbar(){
+    return(
+       <nav className="bg-black w-full h-16 absolute fixed top-0 justify-center items-center gap-8 text-white">
+        <li>Home</li>
+        <li>Event</li>
+        <li>Gallery</li>
+        <li>Register</li>
+        
+       </nav>
+    )
+}
