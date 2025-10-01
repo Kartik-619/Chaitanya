36a119fe458d9b7f30f7237cc0d5251b88831072
Kartik Sharma <poojan4604@gmail.com>
2025-10-01 15:11:43 +0530

    basic structure and home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 
-import './App.css'
+import Home from './pages/home'
 
 function App() {
 
 
   return (
     <>
-      <div>
-       </div>
+      <Home/>
     </>
   )
 }
--- src/component/hero.jsx
+++ src/component/hero.jsx
@@ -1,7 +1,20 @@
-export default function Home(){
-    return(
-        <div className="h-full w-screen bg-black">
+import './hero.css';
+import Sidebar from "./navbar";
+import { useState } from 'react';
+export default function Hero() {
+    const [offset,setOffest]=useState(0);
+  return (
+    <div className="relative w-full min-h-screen overflow-hidden">
+      {/* Background */}
+      <div className="bg"></div>
 
-        </div>
-    )
+      {/* Sidebar */}
+      <Sidebar />
+    
+      {/* Content */}
+      <div className="relative z-10 flex items-center justify-center h-screen text-white">
+        <h1 className="trade-font self-center items-center top-5 text-4xl font-bold">Welcome!</h1>
+      </div>
+    </div>
+  );
 }
