fb9376dcb1aff9e885af5a61fe18c22203be2ea3
Kartik Sharma <poojan4604@gmail.com>
2025-10-29 22:02:12 +0530

    Update navHero.jsx

--- frontend/src/component/navHero/navHero.jsx
+++ frontend/src/component/navHero/navHero.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import './NavHero.css';
-import Typewriter from '../typewriter';
+import './navHero.css';
 
 export default function NavHero() {
     const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
