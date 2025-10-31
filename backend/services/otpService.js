e9cafedd8e738b4130afbbad87420e1130d9b26a
DikshJaswal <dikshitjaswal1922@gmail.com>
2025-10-31 12:02:32 +0530

    Update otpService.js

--- backend/services/otpService.js
+++ backend/services/otpService.js
@@ -9,7 +9,7 @@ const transporter = nodemailer.createTransport({
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS
     },
-});
+}); 
 
 class OTPService {
     constructor() {
